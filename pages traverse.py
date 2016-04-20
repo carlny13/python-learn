5d8c576d7a892c2a90c34b87964916ac5c1b6820
carlny13 <289819611@qq.com>
2016-04-20 12:09:35 +0800

    Update pages traverse.py

--- pages traverse.py
+++ pages traverse.py
@@ -1,3 +1,4 @@
+#py3.5
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_2.html")
@@ -9,3 +10,43 @@ html = '''<a href="some_url">next</a>
 soup = BeautifulSoup(html)
 for a in soup.find_all('a', href=True):
     print ("Found the URL:", a['href'])
+
+# 提取上一页URL
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://detail.zol.com.cn/cell_phone_index/subcate57_0_list_1_0_1_2_0_2.html")
+bsObj = BeautifulSoup(html.read())
+
+pageList=bsObj.find_all("div",{"class":"small-page"})
+for page in pageList:
+    print(page.a['href']) #/页面信息
+    
+# 提取下一页URL
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://detail.zol.com.cn/cell_phone_index/subcate57_list_1.html?")
+bsObj = BeautifulSoup(html.read())
+
+pageList=bsObj.find_all("a",{"class":"small-page-next"})
+for page in pageList:
+    print(page['href']) #/页面信息
+    
+#提取下一页到列表：
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import  re
+
+pages=set()
+def getLinks(pageUrl):
+    global pages
+    html=urlopen("http://detail.zol.com.cn/cell_phone_index/subcate57_list_1.html?"+pageUrl)
+    bsObj=BeautifulSoup(html)
+    for link in bsObj.findAll("a",{"class":"small-page-next"}):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                #我们遇到了新的页面
+                newPage=link.attrs['href']
+                print(newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+getLinks("")
